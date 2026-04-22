bd96bf526e098607bfb90680cccd7fd15cffeee0
Quentin Cook <jackqcook@gmail.com>
2026-04-21 19:01:19 -0600

    there

--- Picasso/Picasso/script.js
+++ Picasso/Picasso/script.js
@@ -1,5 +1,16 @@
 const email = "quentinjacksoncook@gmail.com";
 
+const sculpturesList = document.querySelector(".sculptures-list");
+
+if (sculpturesList) {
+  const shuffledItems = Array.from(sculpturesList.children)
+    .map((item) => ({ item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ item }) => item);
+
+  shuffledItems.forEach((item) => sculpturesList.appendChild(item));
+}
+
 document.querySelectorAll(".btn").forEach((button) => {
   if (button.disabled) {
     return;
